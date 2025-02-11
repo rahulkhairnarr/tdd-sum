7b82ed218e13ad6120fa959c76a5b8585a2901dc
Rahul Khairnar <rahulkhairnarr@gmail.com>
2025-02-11 11:19:37 +0530

    Add support for negetive number and test case for two sum

--- src/sum.ts
+++ src/sum.ts
@@ -1,8 +1,16 @@
-export const add = (numbers: string): number =>{
-    const total = 0
-    numbers = numbers.trim()
-    if (numbers.length === 1) {
-        return parseInt(numbers)
+export const add = (numbers: string): number | Error => {
+  try {
+    const total = 0;
+    numbers = numbers.trim();
+    if (numbers.length > 0 || numbers.length < 2) {
+      const num = parseInt(numbers);
+      if (num < 0) {
+        return new Error(`negative numbers not allowed ${num}`);
+      }
+      return num;
     }
-    return total
-}
+    return total;
+  } catch (error) {
+    return new Error("Unexpected Error");
+  }
+};
--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,20 +1,26 @@
-import { add } from '../src/sum';
+import { add } from "../src/sum";
 
-describe('Testing Add Function', () =>{
-    test('Checking Empty string',() =>{
-        const result = add('');
-        expect(result).toBe(0);
-    });
-    test('Checking with single number',() =>{
-        const result = add('1');
-        expect(result).toBe(1);
-    });
-    test("Checking with single number with some space", () =>{
-        const result = add(" 1 ");
-        expect(result).toBe(1);
-    });
-    test("Single Negative Number", () =>{
-        const result = add("-1");
-        expect(result).toThrow('negative numbers not allowed -1')
-    });
+describe("Testing Add Function", () => {
+  test("Checking Empty string", () => {
+    const result = add("");
+    expect(result).toBe(0);
+  });
+  test("Checking with single number", () => {
+    const result = add("1");
+    expect(result).toBe(1);
+  });
+  test("Checking with single number with some space", () => {
+    const result = add(" 1 ");
+    expect(result).toBe(1);
+  });
+  test("Single Negative Number", () => {
+    const result = add("-1");
+    expect(() => {
+      if (result instanceof Error) throw result;
+    }).toThrow("negative numbers not allowed -1");
+  });
+  test("Add Two number", () =>{
+    const result = add('2 + 4');
+    expect(result).toBe(6)
+  });
 });
