f6a6d942b9a038bb13d72009619f35553226de95
Rahul Khairnar <rahulkhairnarr@gmail.com>
2025-02-11 10:45:18 +0530

    test case for single number

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,8 +1,12 @@
 import { add } from '../src/sum';
 
 describe('Testing Add Function', () =>{
-    test('Checking Empty string', () =>{
+    test('Checking Empty string',() =>{
         const result = add('');
         expect(result).toBe(0);
-    })
+    });
+    test('Checking with single number',() =>{
+        const result = add('1');
+        expect(result).toBe(1);
+    });
 });
